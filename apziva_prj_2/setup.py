6d5ee9579c84f5379a95b70e071e62bd844f908c
Montserrat Diaz Marroquin <montserrat.dmarroquin@gmail.com>
2023-09-13 22:53:06 -0600

    Add files via upload

---
+++ apziva_prj_2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='term deposit subscription',
+    author='Montserrat Diaz Marroquin',
+    license='MIT',
+)
