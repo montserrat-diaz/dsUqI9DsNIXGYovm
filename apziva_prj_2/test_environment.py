797390d3372cd8bd7f014bf8b359daa906359bfe
Montserrat Diaz Marroquin <montserrat.dmarroquin@gmail.com>
2023-09-15 18:00:03 -0600

    Update test_environment.py

--- apziva_prj_2/test_environment.py
+++ apziva_prj_2/test_environment.py
@@ -1,4 +1,8 @@
 import sys
+import warnings
+
+warnings.filterwarnings("ignore")
+np.random.seed(42)
 
 REQUIRED_PYTHON = "python3"
 
@@ -23,3 +27,14 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
+    data = read_data("data/raw/term-deposit-marketing-2020.csv")
+
+    X_train, y_train, X_val, y_val, X_test, y_test = preprocess_data(data, "y")
+    trained_model, cv_score = train_model(X_train, y_train, XGBClassifier)
+    print("Cross Validation Scores:", cv_score)
+    best_params = tune_hyperparameter(X_train, y_train, XGBClassifier)
+    cv_scores_tuned, val_score, f1_val_tuned = evaluate_model(X_train, y_train, X_val, y_val, XGBClassifier, best_params)
+    print(f"Cross-Validation Scores: {cv_scores_tuned:}\nMean F1 Score: {cv_scores_tuned.mean():}\n\nAccuracy on Validation Set: {val_score:}\nF1 Score on Validation Set: {f1_val_tuned:}")
+    test_score, f1_test_tuned = evaluate_final_model(X_train, y_train, X_test, y_test, XGBClassifier, best_params)
+    print(f"\nAccuracy on Test Set: {val_score:}\nF1 Score on Test Set: {f1_val_tuned:}")
